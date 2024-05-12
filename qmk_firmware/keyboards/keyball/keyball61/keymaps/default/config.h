d535c1eab2dc4df5f51c2c3b05c13d82eafed61c
Kei Kamikawa <Code-Hex@users.noreply.github.com>
2024-05-12 14:39:12 +0900

    fixed compile error for default

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/default/config.h
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/default/config.h
@@ -35,5 +35,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define TAP_CODE_DELAY 5
 
+/* This macro indices using https://github.com/sekigon-gonnoc/qmk_firmware */
+#define USE_BMP_V0_FIRMWARE
+
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
 #define AUTO_MOUSE_DEFAULT_LAYER 2
--- qmk_firmware/keyboards/keyball/keyball61/keymaps/default/rules.mk
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/default/rules.mk
@@ -1,3 +1,6 @@
 RGBLIGHT_ENABLE = yes
 
 OLED_ENABLE = yes
+
+# Include for auto mouse layer
+SRC += lib/quantum/pointing_device/pointing_device_auto_mouse.c
