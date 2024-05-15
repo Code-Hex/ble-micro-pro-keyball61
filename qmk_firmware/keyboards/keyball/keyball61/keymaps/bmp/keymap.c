5f1c3e81a08994a8f67d3257c95c769051671c45
Kei Kamikawa <Code-Hex@users.noreply.github.com>
2024-05-15 23:36:51 +0900

    changed copyright

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/bmp/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/bmp/keymap.c
@@ -1,4 +1,4 @@
-/* Copyright 2019 sekigon-gonnoc
+/* Copyright 2024 Kei Kamikawa @codehex
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
